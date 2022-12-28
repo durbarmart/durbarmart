a8acd6ecf0a27c1a03f7277157e454c2b13c114d
JoshiBipin <bipin.ingrails@gmail.com>
2022-12-28 22:45:03 +0545

    sitemap

--- app/Http/Controllers/SitemapXmlController.php
+++ app/Http/Controllers/SitemapXmlController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
-use App\Models\Product;
+use App\Category;
+use App\Product;
 use Illuminate\Http\Request;
 
 
 class SitemapXmlController extends Controller
 {
     public function index() {
-        // $categories = Category::all();
-        $categories = Category::with('subCategoriesXml')->get(['id','name','slug']);
-        $products = Product::get('name','slug');
+        // $categories = Category::all();with('subCategoriesWithSubSub')->
+        $categories = Category::get(['id','name','slug']);
+        $products = Product::get(['name','slug']);
         // dd($products);
         return response()->view('sitemap', [
             'categories' => $categories,
